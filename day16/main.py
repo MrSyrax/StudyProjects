7be08dbbc9eb0e55d3f837344ba4cfacccc44380
MrSyrax <kevin.carrillo89@gmail.com>
2021-10-21 23:59:08 -0700

    'OOP'

--- day16/main.py
+++ day16/main.py
@@ -11,22 +11,16 @@ profit  = cash_machine.profit
 
 while  more_coffe:
     user_choice = input(f'what drink would you like? {drinks}: ')
-<<<<<<< HEAD
     if user_choice == 'off':
         more_coffe = False
         break
     elif user_choice == 'report':
-=======
-   
-    if user_choice == 'report':
->>>>>>> 7cba774d7b86f6fb2bdd98cdd02405ff775d93d1
       print(cash_machine.report())
       print(coffee_machine.report())
     else:
        drink_chosen = choices.find_drink(user_choice)
        resources_sufficient = coffee_machine.is_resource_sufficient(drink_chosen)
        paid = cash_machine.make_payment(drink_chosen.cost)
-<<<<<<< HEAD
 
     if resources_sufficient and paid:
       coffee_machine.make_coffee(drink_chosen)
