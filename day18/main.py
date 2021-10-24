24dce17ee10d595450f61bc44fbbd175eeddb55d
MrSyrax <kevin.carrillo89@gmail.com>
2021-10-24 11:57:54 -0700

    Intermediate

--- day18/main.py
+++ day18/main.py
@@ -1,36 +1,5 @@
-import turtle as t
-import random
+import colorgram
 
+colors = colorgram.extract('hirst_painting.jpg', 6)
 
-t.colormode(255)
-tim = t.Turtle()
-tim.shape('turtle')
-tim.pensize(10)
-tim.speed(0)
-
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    return r,g,b
-
-colors = random_color()
-directions = [0,90,180,270]
-
- 
-
-for _ in range(600):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
-
-
-
-
-
-
-
-
-screen = tim.Screen()
-screen.exitonclick()
-
+print(colors)
