9e2e4f2bff690ae26899270c0554986579756b23
MrSyrax <kevincarrillo89@gmail.com>
2021-10-29 19:58:56 -0700

    intermediate

--- day25/main.py
+++ day25/main.py
@@ -1,14 +1,23 @@
 import csv
 import pandas
+import statistics as s
 
-# with open('C:/Users/karey/Documents/Python/day25/weather_data.csv') as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != 'temp':
-#             temperatures.append(int(row[1]))
+from pandas.core.series import Series
 
 
+# path to data on home pc C:\Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv
+#path to data on gaming pc 'C:/Users/karey/Documents/Python/day25/weather_data.csv'
+data = pandas.read_csv('C:/Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv')   
+# data_dict = data.to_dict()         
+# # print(data_dict)
+# temp_list = data['temp'].to_list()
 
-data = pandas.read_csv('C:/Users/karey/Documents/Python/day25/weather_data.csv')            
-print(data)
+
+# total_temp = round(data['temp'].mean())
+# max_temp = data['temp'].max()
+
+# print(total_temp)
+# print(max_temp)
+# print(type(data))
+# print(data)
+print(data[data['day'] == 'Monday'])
