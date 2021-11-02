3b3b129d7c40dfe6acb7b21d6611f7a18bbb5504
MrSyrax <kevin.carrillo89@gmail.com>
2021-11-01 22:26:56 -0700

    intermediate

--- day25/main.py
+++ day25/main.py
@@ -1,23 +1,45 @@
-import csv
+import turtle as t
 import pandas
-import statistics as s
 
-from pandas.core.series import Series
+screen = t.Screen()
+screen.title('U.S. States Game')
+image = 'C:/Users/karey/Documents/Python/day25/blank_states_img.gif'
+screen.addshape(image)
+t.shape(image)
+states = pandas.read_csv('C:/Users/karey/Documents/Python/day25/50_states.csv')
+
+state_to_list = len(states['state'].to_list())
+
+correct = 0
+user_answer = screen.textinput(title='Guess the State', prompt="What's another state")
+game_is_on = True
+while game_is_on:
+    
+    user_answer = user_answer.capitalize()
+    us_states = states['state'].to_list()
+    lenth_of_list = len(us_states)
 
 
-# path to data on home pc C:\Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv
-#path to data on gaming pc 'C:/Users/karey/Documents/Python/day25/weather_data.csv'
-data = pandas.read_csv('C:/Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv')   
-# data_dict = data.to_dict()         
-# # print(data_dict)
-# temp_list = data['temp'].to_list()
+    if user_answer == 'Exit':
+        game_is_on = False
 
 
-# total_temp = round(data['temp'].mean())
-# max_temp = data['temp'].max()
+    if user_answer in us_states:
+        us_states = states[states['state'] == user_answer]
+        x_of_state = us_states['x']
+        y_of_state = us_states['y']
+        correct+=1
+        state_name = t.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        state_name.goto(int(x_of_state), int(y_of_state))
+        state_name.write(user_answer.capitalize())
 
-# print(total_temp)
-# print(max_temp)
-# print(type(data))
-# print(data)
-print(data[data['day'] == 'Monday'])
+    elif user_answer not in us_states:
+        state_name = t.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        state_name.goto(0,300)
+        state_name.write('you suck! try again!')
+
+    user_answer = screen.textinput(title=f'{correct}/{lenth_of_list} States Correct', prompt="What's another state")
