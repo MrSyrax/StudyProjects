5245107f4b453daa835fa9465cdab705a164bf81
MrSyrax <kevincarrillo89@gmail.com>
2021-11-03 10:33:16 -0700

    Intermediate

--- day25/main.py
+++ day25/main.py
@@ -8,16 +8,8 @@ from pandas.core.series import Series
 # path to data on home pc C:\Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv
 #path to data on gaming pc 'C:/Users/karey/Documents/Python/day25/weather_data.csv'
 data = pandas.read_csv('C:/Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv')   
-# data_dict = data.to_dict()         
-# # print(data_dict)
-# temp_list = data['temp'].to_list()
 
+avg = round(data['temp'].mean())
+max_temp = data['temp'].max()
 
-# total_temp = round(data['temp'].mean())
-# max_temp = data['temp'].max()
-
-# print(total_temp)
-# print(max_temp)
-# print(type(data))
-# print(data)
-print(data[data['day'] == 'Monday'])
+print(f'average temp for the week is: {avg}\nThe Max temp for the week was: {max_temp}')
---
+++ day26/main.py
@@ -0,0 +1,43 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
+
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     student = key
+#     score = value
+
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keycode Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+import pandas
+
+phonetic_alphabet = pandas.read_csv('C:/Users/Kareyo/Documents/Python/StudyProjects/day26/nato_phonetic_alphabet.csv')
+pa_dict = {code.letter:code.code for (index, code) in phonetic_alphabet.iterrows()}
+
+
+#TODO 2. Create a list of the phonetic code codes from a word that the user inputs.
+
+guessing_names = True
+while guessing_names:
+
+    user_input = input('Enter a code: ')
+    if user_input != 'Exit':
+
+        phonetic_list = [pa_dict[n.upper()] for n in user_input]
+        print(phonetic_list)
+    else:
+        break
