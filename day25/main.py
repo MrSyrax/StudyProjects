1a1bcb36f7cfeb3f8428cec84602ac503992d998
MrSyrax <kevincarrillo89@gmail.com>
2021-11-04 15:11:47 -0700

    'Intermediate'

--- day25/main.py
+++ day25/main.py
@@ -9,7 +9,6 @@ t.shape(image)
 states = pandas.read_csv('C:/Users/karey/Documents/Python/day25/50_states.csv')
 
 
-<<<<<<< HEAD
 # path to data on home pc C:\Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv
 #path to data on gaming pc 'C:/Users/karey/Documents/Python/day25/weather_data.csv'
 data = pandas.read_csv('C:/Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv')   
@@ -18,59 +17,3 @@ avg = round(data['temp'].mean())
 max_temp = data['temp'].max()
 
 print(f'average temp for the week is: {avg}\nThe Max temp for the week was: {max_temp}')
-=======
-us_states = states['state'].to_list()
-guessed_states = []
-
-correct = 0
-user_answer = screen.textinput(title='Guess the State', prompt="guess a state")
-
-while guessed_states < 50:
-    user_answer = user_answer.capitalize()
-    lenth_of_list = len(us_states)
-    
-
-    if user_answer == 'Exit':
-        game_is_on = False
-
-        missing_states = [state for state in us_states if state not in guessed_states]
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv('C:/Users/karey/Documents/Python/day25/missed.csv')
-        break
-
-
-    if user_answer in us_states:
-        guessed_states.append(user_answer)
-       
-        state_name = t.Turtle()
-        state_name.hideturtle()
-        state_name.penup()
-        x_and_y = states[states['state'] == user_answer]
-        x_of_state = x_and_y['x']
-        y_of_state = x_and_y['y']
-        correct+=1
-        state_name.goto(int(x_of_state), int(y_of_state))
-        state_name.write(user_answer.capitalize())
-        
-
-    elif user_answer not in us_states:
-        state_name = t.Turtle()
-        state_name.hideturtle()
-        state_name.penup()
-        state_name.goto(0,300)
-        state_name.write('you suck! try again!')
-
-    user_answer = screen.textinput(title=f'{correct}/{lenth_of_list} States Correct', prompt="What's another state")
-
-<<<<<<< HEAD
-=======
-m_states = {
-    'missed': us_states
-}
-
-missed_states = pandas.DataFrame(m_states)
-missed_states.to_csv('C:/Users/karey/Documents/Python/day25/missed.csv')
-
-print(f'great job, but you missed {us_states}')
->>>>>>> 3f10f53a1deab949e0dc440c9c175ae9c2ec5f29
->>>>>>> d2e75dd80f7aacae961c5549b3c76e5b53d9c47e
--- day26/main.py
+++ day26/main.py
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-# numbers = [1,2,3]
-# new_numbers = [n + 1 for n in numbers]
-
-# print(new_numbers)
-
-# name = 'Angela'
-# new_list = [letter for letter in name]
-# print(new_list)
-
-
-# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-# short_names = [n.upper() for n in names if len(n)>4]
-
-# print(short_names)
-
-# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-# result = [n for n in numbers if n%2==0]
-
-# print(result)
-
-
-
-# new_dict = {new_key:new_value for item in list}
-
-# new_dict = {new_key:new_value for (key,value) in dict.items() }
-
-# new_dict = {new_key:new_value for (key,value) in dict.items() if test}
-
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-
-import random
-
-score_list = {student:random.randint(1,100) for student in names}
-
-print(score_list)
-
-passed_student = {student: score for (student, score) in score_list.items() if score >= 60}
-
-print(passed_student)
-
-
-
-=======
 # student_dict = {
 #     "student": ["Angela", "James", "Lily"], 
 #     "score": [56, 76, 98]
@@ -86,4 +41,3 @@ while guessing_names:
         print(phonetic_list)
     else:
         break
->>>>>>> d2e75dd80f7aacae961c5549b3c76e5b53d9c47e
