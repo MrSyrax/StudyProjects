7cfb94fe2b7fd5eb5f3dde15a93b1879a24615ba
MrSyrax <kevin.carrillo89@gmail.com>
2021-11-03 21:38:33 -0700

    intermediate

---
+++ day27/main.py
@@ -0,0 +1,28 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('My First GUI Program')
+window.minsize(width=500, height=300)
+
+
+
+
+
+#label
+my_label = tkinter.Label(text='I Am a Label', font=('Arial',24,'bold'))
+my_label.pack(expand=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
