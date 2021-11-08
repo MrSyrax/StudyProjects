7d13f767ad4357cd9b4b0820113f9df2676b8556
MrSyrax <kevin.carrillo89@gmail.com>
2021-11-08 13:07:53 -0800

    Intermediate

--- day28/main.py
+++ day28/main.py
@@ -102,7 +102,7 @@ check_marks.grid(column=1,row=4)
 
 #create canvas where the tomato picture will go
 canvas = Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file='C:/Users/Kareyo/Documents/Python/StudyProjects/day28/tomato.png')
+tomato_img = PhotoImage(file='C:/Users/Karey/Documents/Python/day28/tomato.png')
 canvas.create_image(100,112, image=tomato_img)
 timer_text = canvas.create_text(100,130, text='00:00', fill='white',font=(FONT_NAME,35,'bold'))
 canvas.grid(column=1,row=1)
--- day29/main.py
+++ day29/main.py
@@ -1,13 +1,61 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_new_pw():
+    #Password Generator Project
+    
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+    password_list = [choice(letters) for _ in range(randint(8, 10))]
+
+    password_list+= [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list+= [choice(numbers) for _ in range(randint(2, 4))]
+   
+    shuffle(password_list)
+
+   
+    password = ''.join(password_list)
+
+    entry_field = password_entry.get()
+
+    if len(entry_field) == 0:
+        password_entry.insert(END,f'{password}')
+        pyperclip.copy(password_entry.get())
+        pyperclip.paste()
+    else:
+        password_entry.delete(0,END)
+        password_entry.insert(END,f'{password}')
+        pyperclip.copy(password_entry.get())
+        pyperclip.paste()   
+
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_data():
+
     web = website_entry.get()
     em = email_entry.get()
     pw = password_entry.get()
-    with open('C:/Users/Kareyo/Documents/Python/StudyProjects/day29/credentials.txt', 'a') as file:
-        file.write(f'{web} | {em} | {pw}\n')
+    if len(web) == 0 or len(pw)== 0:
+        messagebox.showinfo(title='Oops', message='Please do not leave any fields emtpy')
+    else:
+        is_ok = messagebox.askokcancel(title=web, message=f'These are the details entered: \nEmail: {em} '
+                                                    f'\nPassword{pw} \nIs it ok to save?')
+        if is_ok:
+            with open('C:/Users/karey/Documents/Python/day29/credentials.txt', 'a') as file:
+                file.write(f'{web} | {em} | {pw}\n')
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+            website_entry.focus()
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -15,7 +63,7 @@ window.title('Password Manager')
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200,height=200)
-lock_image = PhotoImage(file='C:/Users/Kareyo/Documents/Python/StudyProjects/day29/logo.png')
+lock_image = PhotoImage(file='C:/Users/karey/Documents/Python/day29/logo.png')
 canvas.create_image(100,100, image=lock_image)
 canvas.grid(row=0,column=1)
 
@@ -26,17 +74,25 @@ email_lable.grid(row=2,column=0)
 password_label = Label(text='Password:')
 password_label.grid(row=3,column=0)
 
+#creates the 'website entry with a width of 52
 website_entry = Entry(width=52)
+#places the website entry in row 1 column 1 with a columnspan of 2 (2 "cells")
 website_entry.grid(row=1, column=1, columnspan=2)
+#places the cursor on the website entry line
 website_entry.focus()
+
 email_entry = Entry(width=52)
+#inserts "default text" into the entry line
 email_entry.insert(END,'Kevincarrillo89@gmail.com')
 email_entry.grid(row=2,column=1, columnspan=2)
+
 password_entry = Entry(width=33)
 password_entry.grid(row=3,column=1)
 
-generate_password_button = Button(text='Generate Password')
+#creates the generate button with and it says "generate password" on the button
+generate_password_button = Button(text='Generate Password', command=generate_new_pw)
 generate_password_button.grid(row=3,column=2)
+
 add_button = Button(text='Add',width=44,command=save_data)
 add_button.grid(row=4,column=1, columnspan=2)
 
