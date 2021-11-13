60f4fbc9bfb5d5a34994f1b0b7d57ecec55a92d9
MrSyrax <kevin.carrillo89@gmail.com>
2021-11-12 23:09:25 -0800

    intermediate

--- day32/main.py
+++ day32/main.py
@@ -1,46 +1,39 @@
-# import smtplib
-
-# # with open('C:/Users/karey/Documents/Python/day32/quotes.txt') as file:
-# #     new_file = file.readlines()
-
-# # print(new_file)
-
-# my_email = 'kevinlearningpython@gmail.com'
-# password = '5426233Kk!!'
-
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email,password=password)
-#     connection.sendmail(
-#         from_addr=my_email, 
-#         to_addrs='kevincarrillo89@yahoo.com', 
-#         msg='Subject:Hello/n/nThis is the body of my email!')
-
-import datetime as dt
+##################### Extra Hard Starting Project ######################
+from datetime import datetime
+import pandas
+import random
 import smtplib
-from random import choice
 
 
 my_email = 'kevinlearningpython@gmail.com'
 password = '5426233Kk!!'
 
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
+date = datetime.now()
+date_to_check = (date.month,date.day)
+
+# 2. Check if today matches a birthday in the birthdays.csv
+data = pandas.read_csv('birthdays.csv')
+birthdays = {index:data_row for (index, data_row) in data.iterrows()}
+
+checker = 0
+for n in range(len(birthdays)):
+    dates = (birthdays[n]['month'], birthdays[n]['day'])
+    if dates == date_to_check:
+        name = birthdays[n]['name']
+        email = birthdays[n]['email']
+        checker+=1
+    
 
 
-if day_of_week == 4:
-    with open('C:/Users/karey/Documents/Python/day32/quotes.txt') as quote_file:
-        all_quotes = quote_file.readlines()
-        quote_of_the_day = choice(all_quotes)
+if checker !=0:
+    letters = f'letter_templates/letter_{random.randint(1,3)}.txt'
+    with open(letters) as letter:
+        birthday_letter = letter.read()
+        final_letter = birthday_letter.replace('[NAME]', name)
     with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email, to_addrs='jasminecarrillo1207@gmail.com', msg=f'Subject:Have a great Day!\n\n{quote_of_the_day}')
-
-
-
-# date_of_birth = dt.datetime(year=1989 ,month=7 , day=12, hour=4)
-
-# print(date_of_birth)
+        connection.login(my_email,password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email,
+            msg=F'Subject:Happy Birthday!\n\n{final_letter}')
