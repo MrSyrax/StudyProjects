88a487f543b473df0bc603b4902e3189508b658e
MrSyrax <kevincarrillo89@gmail.com>
2021-11-16 10:11:09 -0800

    intermediate

--- day25/main.py
+++ day25/main.py
@@ -9,9 +9,9 @@ t.shape(image)
 states = pandas.read_csv('C:/Users/karey/Documents/Python/day25/50_states.csv')
 
 
-# path to data on home pc C:\Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv
+# path to data on home pc C:/Users/Kareyo/Documents/Python/StudyProjects/day25/weather_data.csv
 #path to data on gaming pc 'C:/Users/karey/Documents/Python/day25/weather_data.csv'
-data = pandas.read_csv('C:/Users\Kareyo\Documents\Python\StudyProjects\day25\weather_data.csv')   
+data = pandas.read_csv('C:/Users/Kareyo/Documents/Python/StudyProjects/day25/weather_data.csv')   
 
 avg = round(data['temp'].mean())
 max_temp = data['temp'].max()
