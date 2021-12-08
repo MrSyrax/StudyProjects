4e7dc14ff6067d3f7cc266bae0a6dd684da8ae92
MrSyrax <kevin.carrillo89@gmail.com>
2021-12-08 02:50:51 -0800

    web

--- day45 - web scraping/main.py
+++ day45 - web scraping/main.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 
-with open("day45 - web scraping/website.html", encoding="utf8") as file:
+with open('day45 - web scraping\website.html', encoding='utf8') as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
 
-anchor_tags = soup.find_all(name='a')
-
-print(len(anchor_tags))
+print(soup.title)
